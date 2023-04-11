62715963c838ca6460d25096fa5e5f00d9be0088
1 <sergey.trbv@yandex.ru>
2023-04-12 00:21:12 +0400

    branch2

--- src/test/java/com/example/testing/service/UserTest.java
+++ src/test/java/com/example/testing/service/UserTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
-    private static String mail ;
+    private static String mail;
     private static String login;
 
     @BeforeAll
@@ -18,24 +18,28 @@ class UserTest {
         login = "log";
     }
 
-    static User user = new User(mail,login);
+    static User user = new User(mail, login);
 
-//Создайте тест, который выполнит тестирование создания объекта User с передачей в него двух параметров. Такая функция лишь должна проверять устанавливаются ли данные при создании объекта;
+    //Создайте тест, который выполнит тестирование создания объекта User с передачей в него двух параметров. Такая функция лишь должна проверять устанавливаются ли данные при создании объекта;
     @Test
-    void checkingObjectInitialization() {
-        Assertions.assertNotNull(user);
+    void checkingObjectInitializationWithParameters() {
+        User user1 = new User("aaa@aaa.ry", "login");
+        Assertions.assertEquals("aaa@aaa.ry", user1.getMail());
+        Assertions.assertEquals("login", user1.getLogin());
     }
 
-//Создайте аналогичный тест, который выполнит тестирование по созданию объекта без передачи в него параметров;
+    //Создайте аналогичный тест, который выполнит тестирование по созданию объекта без передачи в него параметров;
+    @Test
+    void checkingObjectInitializationWithoutParameters() {
+    }
 
-
-//Создайте тест, который протестирует установлен ли корректный Email адрес в поле email в классе User. Некорректным будет считаться тот email, в котором нет знака @ или же знака точки
+    //Создайте тест, который протестирует установлен ли корректный Email адрес в поле email в классе User. Некорректным будет считаться тот email, в котором нет знака @ или же знака точки
     @Test
     void thePresenceOfASignInTheObject() {
         Assertions.assertTrue(user.validateChar(mail));
     }
 
-//Создайте тест, который определяет, равны ли login и email (Они не должны быть равны).
+    //Создайте тест, который определяет, равны ли login и email (Они не должны быть равны).
     @Test
     void comparingLoginAndMail() {
         Assertions.assertNotEquals(mail, login);
